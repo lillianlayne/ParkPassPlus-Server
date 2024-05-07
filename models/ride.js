aea7600f8925904d01af8e18829e5b6f56b834d3
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-07 16:23:02 -0400

    ride model

--- models/ride.js
+++ models/ride.js
@@ -6,7 +6,7 @@ const RideSchema = new Schema({
   description: String,
   location: String,
   inches: Number, // height requirement in inches
-  image: String,
-
+  photoUrl: String,
+}, {timestamps: true});
 
 module.exports = mongoose.model("Ride", RideSchema);
